c857ccb7bd4c3a5c64f015dd958cc2519aa2b45c
Willian Metzger <willianmetzger@gmail.com>
2020-03-27 14:15:19 -0300

    Reorganização; Game.gd; Level.gd
    Reorganização das pastas, renomeação do projeto, criação e implementação básica das classes game e level

---
+++ src/Levels/Game.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var battle_background: = "res://assets/coin.png"
+
+func _ready() -> void:
+	load(battle_background)
---
+++ src/Levels/Level.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+export var background_img_path: String
+
+ 
